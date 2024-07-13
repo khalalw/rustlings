79d2efd482aacbb37487ab93b7c186b6c1b18223
Khalal Walker <khalalw@outlook.com>
2024-07-12 22:03:03 -0700

    done with clippy

--- exercises/22_clippy/clippy1.rs
+++ exercises/22_clippy/clippy1.rs
@@ -5,8 +5,9 @@
 // warnings. Check Clippy's suggestions from the output to solve the exercise.
 
 fn main() {
+    use std::f32::consts::PI;
     // TODO: Fix the Clippy lint in this line.
-    let pi = 3.14;
+    let pi = PI;
     let radius: f32 = 5.0;
 
     let area = pi * radius.powi(2);
